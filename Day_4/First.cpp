a59e2ff20c77b5e74ae75a329971531a6b87a2ab
Chetan Patil <chetanpatil6303@gmail.com>
2025-01-27 22:31:21 +0530

    update

--- Day_4/First.cpp
+++ Day_4/First.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
+    int a=16;
+    if (a==16)
+    {
+        printf("A is 16");
+    }
 
+    if (a>10)
+    {
+        cout<<"\nA is greater than 10";
+    }
+    else{
+        cout<<"A is less than 10";
+    }
+    
+    
 }
 
 //Conditional statements 
