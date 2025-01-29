4aacfaa85e1f0d93cfb1eb2b2a05afd56084aa03
Chetan Patil <chetanpatil6303@gmail.com>
2025-01-29 23:48:57 +0530

    Update

--- Day_4/Second.cpp
+++ Day_4/Second.cpp
@@ -6,4 +6,31 @@ int main()
     int n;
     cout<<"Enter number Week day";
     cin>>n;
+
+    switch (n)
+    {
+    case 1:
+        cout<<"Sunday";
+        break;
+    case 2:
+        cout<<"Monday";
+        break;
+    case 1:
+        cout<<"Tuesday";
+        break;
+    case 1:
+        cout<<"Wednsday";
+        break;
+    case 1:
+        cout<<"Thursday";
+        break;
+    case 1:
+        cout<<"Friday";
+        break;
+    case 1:
+        cout<<"Saturday";
+        break;
+    default:
+        break;
+    }
 }
