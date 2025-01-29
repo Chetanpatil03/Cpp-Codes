4e2e2bda02913fd2a39fef2f1e0dc49c60671596
Chetan Patil <chetanpatil6303@gmail.com>
2025-01-29 23:54:55 +0530

    Created third.cpp

---
+++ Day_4/Third.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10,b=15,c;
+    cout<<"Enter number";
+    cin>>c;
+
+    if (a>b&&a>c)
+    {
+        if (a>=15)
+        {
+            cout<<"A is largest number and it is also greater than 15";
+        }
+        else
+        {
+            cout<<"A is largest number but it is smaller than 15";
+        }
+        
+        
+    }
+    
+}
