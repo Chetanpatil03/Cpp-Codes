e5a4d18156e9c017366260cb06ae35f8c20ba8e3
Chetan Patil <chetanpatil6303@gmail.com>
2025-02-02 23:21:29 +0530

    Created second

---
+++ Day_5/Second.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=1;
+    while (a>10)
+    {
+        while (1<5)
+        {
+            cout<<"Less than 5";
+        }
+        
+        cout<<"Hello"<<endl;
+    }
+    
+}
