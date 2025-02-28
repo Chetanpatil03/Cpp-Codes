e2ba2a919fd1562c7b9e2aba67482cef7c05e02e
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-01 00:57:41 +0530

    created ClassObject.cpp used to demonstrate the use of class and objects

---
+++ OOPs/ClassObjects.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Students
+{
+    public:
+        int roll;
+        string name;
+        int age;
+        bool gender;
+};
+
+int main()
+{
+    Students chetan;
+    chetan.name = "Chetan";
+    chetan.age = 21;
+    chetan.gender = 1;
+    chetan.roll = 1;
+
+}
