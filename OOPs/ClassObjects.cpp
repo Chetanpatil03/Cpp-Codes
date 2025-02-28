5b97697a23b17da078ae02766785d2729b0a7cc6
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-01 01:03:59 +0530

    update

--- OOPs/ClassObjects.cpp
+++ OOPs/ClassObjects.cpp
@@ -17,4 +17,25 @@ int main()
     chetan.gender = 1;
     chetan.roll = 1;
 
+    Students prafull;
+    prafull.name = "Prafull";
+    prafull.age = 21;
+    prafull.gender = 1;
+    prafull.roll = 38;
+
+    cout<<"First Student : "<<chetan.name<<endl;
+    cout<<"Second Student : "<<prafull.name<<endl;
+
+    cout<<"Age 1 : "<<chetan.age<<endl;
+    cout<<"Age 2 : "<<prafull.age<<endl;
+
+    if (chetan.gender)
+    {
+        cout<<"First student is male";
+    }else{
+        cout<<"First student is female";
+    }
+    
+
+
 }
