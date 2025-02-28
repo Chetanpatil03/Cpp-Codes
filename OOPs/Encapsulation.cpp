52f9e29b4c9beec00283f956f9c91ee6e8982e7b
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-01 01:30:51 +0530

    added encapsulation code

--- OOPs/Encapsulation.cpp
+++ OOPs/Encapsulation.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+class App{ //not a encapsulation because there is public data members every one can access and modify the data members.
+    public:
+        int p,q;
+
+    public: void setP(int input){
+        p = input;
+        q = p/2;
+    }
+};
+
 int main(){
-    cout<<"Encapsulation";
+    // App obj;
+    // obj.p = 6;
+    // obj.q=12;
+    // cout<<obj.q;
+    
 }
