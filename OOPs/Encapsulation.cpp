c6f756140d6e224c9ef17d9cb6e3797a053958ad
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-01 01:43:45 +0530

    Update Encapsulation added code of encapsulation

--- OOPs/Encapsulation.cpp
+++ OOPs/Encapsulation.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class App{ //not a encapsulation because there is public data members every one can access and modify the data members.
+class FalseApp{ //not a encapsulation because there is public data members every one can access and modify the data members.
     public:
         int p,q;
 
@@ -11,10 +11,31 @@ class App{ //not a encapsulation because there is public data members every one 
     }
 };
 
+class App{ 
+    private: int p;
+    private: int q; //rule of the encapsulation data members should private.
+
+    public: void setP(int input){
+        p = input;
+        q = p/2;
+
+        cout<<"Value of Q : "<<q;
+    }
+};
+
 int main(){
-    // App obj;
+    // FalseApp obj;
     // obj.p = 6;
     // obj.q=12;
     // cout<<obj.q;
-    
+
+    // App obj;
+    // obj.p = 6;
+    // obj.q=12; this will give error because we can't access the private data members.
+    // cout<<obj.q;
+
+    App obj;
+    obj.setP(8); // this is real encapsulation you can't access the private data members only member functions.
+    return 0;
+
 }
