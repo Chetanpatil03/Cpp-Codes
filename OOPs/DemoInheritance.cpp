be22a7382fc9c2794aa64c6076a9d32348a96189
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-02 01:04:19 +0530

    update added the walk method in code

--- OOPs/DemoInheritance.cpp
+++ OOPs/DemoInheritance.cpp
@@ -5,6 +5,9 @@ class Animal{
     public: void Eat(){
         cout<<"Animal is Eating................"<<endl;
     }
+    public: void Walk(){
+        cout<<"Animal is Walking................"<<endl;
+    }
 };
 
 class Dog:public Animal{
@@ -26,13 +29,16 @@ int main(){
 
     // With dog and cat class objects we can access the properties of Animal class.
     d.Eat();
+    d.Walk();
     c.Eat();
+    c.Walk();
+    
 
     // The Cat, Dog and Animal class can access there member functions.
     d.Bark();
     c.Meow();
     a.Eat();
 
-    
+
 
 }
