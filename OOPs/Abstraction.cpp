e1f6dcbb4859ca3a8aa8bf7d74491a43cba084da
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-02 01:16:14 +0530

    Update

--- OOPs/Abstraction.cpp
+++ OOPs/Abstraction.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class App{ 
+    private: int p;
+    private: int q; 
+
+    public: void setP(int input){ //user will access this block 
+        p = input;
+        q = p/2;
+
+        cout<<"Value of Q : "<<q;
+    }//hide the implementation code only show the essential information.
+};
+
 int main(){
-    cout<<"Abstraction";
+    App obj;
+    obj.setP(8);
     return 0;
+
 }
