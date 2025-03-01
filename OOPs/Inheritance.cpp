adf1a25e8d2ccfdde35b85c2c13924248c2b4ff0
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-02 00:25:30 +0530

    created Inheritance.cpp and update the notes

---
+++ OOPs/Inheritance.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Parent{
+    public:
+        int p;
+};
+
+class Child : public Parent{
+    public:
+        int c;
+};
+
+int main()
+{
+    
+}
