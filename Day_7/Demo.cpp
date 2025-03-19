be710065f05bccf0df32230b95873e7b80ad7cf7
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-20 01:41:37 +0530

    checking wheather the for each loop working on basic array or not

--- Day_7/Demo.cpp
+++ Day_7/Demo.cpp
@@ -1,1 +1,15 @@
-#include
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int Elements[] = {125,2,6,22,6,5,26,3,19,5,62,2,6};
+
+    for (int elem : Elements)
+    {
+        cout<<elem<<" ";
+    }
+    
+    return 0;
+}
