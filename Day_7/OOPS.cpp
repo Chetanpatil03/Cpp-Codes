5e4055d79a0e9bb51bdbf80494296ef4afd1749f
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-20 01:38:35 +0530

    update cpp

--- Day_7/OOPS.cpp
+++ Day_7/OOPS.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-class Demo{
+class chai{
+    //data members attributes
     // Creating class and its members
-    public:
-        int a=25;
+    // public:
+    //     int a=25;
+
+    string teaName; //name of the tea
+    int servings;  //number of serveins
+    vector<string> ingridients;  //list of ingridients to make tea
+
+    //member functions
+    void displayChaiDetails(){
+        cout<<"Tea name : "<<teaName<<endl;
+        cout<<"Servings : "<<servings<<endl;
+        cout<<"Ingridients : ";
+        for (string ingridient : ingridients)
+        {
+            cout<<" "<<ingridient;
+        }
+        cout<<endl;
+        
+    }
 };
 
 int main()
