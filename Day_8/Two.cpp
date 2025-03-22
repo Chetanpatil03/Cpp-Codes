d666a7f1ad47bde883170f8b0746fec95ddb28d4
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-23 00:58:39 +0530

    created two.cpp

---
+++ Day_8/Two.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // cout<<"Hello world!";
+
+    int chaiServerd [7] ={51,50,85,90,88,92,89};
+
+    cout<<"Chai Servered on day 1  : "<<chaiServerd[0]<<endl;
+    cout<<"Chai Servered on day 2  : "<<chaiServerd[1]<<endl;
+    
+
+    return 0;
+   
+}
