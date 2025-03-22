aa18c106512c91e7449de38ac179235c9c11a49c
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-22 23:38:54 +0530

    updated code of array.cpp

--- Day_8/Arrays.cpp
+++ Day_8/Arrays.cpp
@@ -4,4 +4,14 @@ using namespace std;
 
 int main(){
     cout<<"Hello world!";
+    int arr[] = {15,565,69,65,96,896,65};
+    int size = sizeof(arr)/sizeof(int);
+    cout<<size;
+    int sum = 0;
+
+    for(int i: arr){
+        sum = sum + i;
+    }
+
+    cout<<"Sum of array : "<<sum;
 }
