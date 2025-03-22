5ea9613c2c1e676675a9e6fc2e1affe8e8155363
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-23 00:55:29 +0530

    update printing array elements

--- Day_8/Arrays.cpp
+++ Day_8/Arrays.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello world!";
+    // cout<<"Hello world!";
+
+    int chaiTemperature [5] ={85,90,88,92,89};
+
+    cout<<"Chai Temperatures : "<<endl;
+
+    for (int i=0;i<5;i++)
+    {
+        cout<<chaiTemperature[i]<<endl;
+    }
+    
+
+    return 0;
    
 }
