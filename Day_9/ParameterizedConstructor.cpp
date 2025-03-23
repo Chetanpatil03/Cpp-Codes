f92d646729a66a4f793f92dcee638786c6d9bcdc
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-24 01:35:32 +0530

    code of Parameterized constructor in which constructor taking three parameters at the time of object creation

---
+++ Day_9/ParameterizedConstructor.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Chai
+{
+    public :
+        string teaName;
+        int servings;
+        vector<string> ingredients;
+
+        //parameterized constructor;
+        Chai(string name,int serv, vector<string> ingre){
+
+            teaName = name;
+            servings = serv;
+            ingredients = ingre;
+
+            cout<<"Parameterized Constructor called"<<endl;
+
+
+        }
+    
+        void displayChaiDetails(){
+            cout<<"Tea name : "<<teaName<<endl;
+            cout<<"Servings : "<<servings<<endl;
+            cout<<"Ingridients : ";
+
+            for (string ingredient : ingredients)
+            {
+                cout<<" "<<ingredient;
+            }
+            cout<<endl;
+        }
+
+};
+
+
+int main(){
+    // cout<<"Constructors in cpp";
+    Chai lemonTea("Lemon Tea",2,{"Water","Tea leaves","Lemon","Honey"});
+
+    lemonTea.displayChaiDetails();
+    return 0;
+
+}
