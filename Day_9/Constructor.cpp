a27390820c74035a2db5aa36d99af0cd8fdef452
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-24 01:23:37 +0530

    Default constructor

--- Day_9/Constructor.cpp
+++ Day_9/Constructor.cpp
@@ -1,8 +1,35 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+class Chai
+{
+    public :
+        string teaName;
+        int servings;
+        vector<string> ingredients;
+
+        Chai(){
+            cout<<"Constructor called";
+        }
+    
+        void displayChaiDetails(){
+            cout<<"Tea name : "<<teaName<<endl;
+            cout<<"Servings : "<<servings<<endl;
+            cout<<"Ingridients : ";
+
+            for (string ingredient : ingredients)
+            {
+                cout<<" "<<ingredient;
+            }
+            cout<<endl;
+        }
+
+};
+
 
 int main(){
     cout<<"Constructors in cpp";
-
+    Chai chai;
     return 0;
 }
