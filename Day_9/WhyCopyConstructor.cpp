795b4f9eb69c354289c8a52f032d7338eb6ff4ae
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-25 00:28:52 +0530

    created copyconstructor.cpp

--- Day_9/WhyCopyConstructor.cpp
+++ Day_9/WhyCopyConstructor.cpp
@@ -9,29 +9,18 @@ class Chai
         int servings;
         vector<string> ingredients;
 
-        //parameterized constructor;
+        //its all good until pointers are not arrieved.
+
+        //Copy constructor;
         Chai(string name,int serv, vector<string> ingre){
 
             teaName = new string(name);
             servings = serv;
             ingredients = ingre;
 
-            cout<<"Constructor called"<<endl;
+            cout<<"Copy Constructor called"<<endl;
 
 
-        }
-
-        Chai(Chai & other){
-            teaName = new string(*other.teaName);
-            servings = other.servings;
-            ingredients = other.ingredients;
-
-            cout<<"Copy constructor is called"<<endl;
-        }
-
-        ~Chai(){
-            delete teaName;
-            cout<<"Distructor called"<<endl;
         }
     
         void displayChaiDetails(){
@@ -46,18 +35,35 @@ class Chai
             cout<<endl;
         }
 
+        ~Chai(){
+            delete teaName;
+            cout<<"Distructor has been called";
+        }
 };
 
 
 int main(){
     // cout<<"Constructors in cpp";
     Chai lemonTea("Lemon Tea",2,{"Water","Tea leaves","Lemon","Honey"});
+
     lemonTea.displayChaiDetails();
 
-    //copy the object
-    Chai copiedChai = lemonTea;
+    Chai copiedChai = lemonTea; //makes copy of the original object
     copiedChai.displayChaiDetails();
+
+    // lemonTea.teaName = "Modified lemon tea";  //update in only origanal not on copied.
+
+    // cout<<"lemon tea"<<endl;
+    // lemonTea.displayChaiDetails(); 
+    // cout<<"Copied Tea : "<<endl;
+    // copiedChai.displayChaiDetails();
+
     
+
+
+
     return 0;
 
 }
+
+// 
