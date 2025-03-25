0740c48b8a19fd52286ab26917fef9bb694dad73
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-26 01:40:59 +0530

    update added getter and setter method for some properties of the class

--- Day_11/First.cpp
+++ Day_11/First.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 using namespace std;
 
@@ -19,9 +20,38 @@ class Chai{
             servings = serv;
             ingredieants = ingre;
         }
+
+        //getter and setter methods
+
+        string getTeaName(){
+            return teaName;
+        }
+
+        void setTeaName(string name){
+            teaName = name;
+            //logic here like tea name in uppercase or lowercase
+
+        }
+
+        int getServings(){
+            return servings;
+        }
+
+        void setServingns(int serv){
+            servings = serv;
+        }
+
+        
 };
 
 int main(){
-    cout<<"Getter and setter methods";
+    cout<<"Getter and setter methods"<<endl;
+    
+
+    Chai ch ;
+    
+    ch.setTeaName("HCnnu");
+    cout<<ch.getTeaName();
+
     return 0;
 }
