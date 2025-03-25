1628ead34c23404622535025f1b04f63ab036354
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-26 02:12:28 +0530

    update

--- Day_11/Delegation.cpp
+++ Day_11/Delegation.cpp
@@ -23,6 +23,8 @@ class Chai{
 
         Chai(string name): Chai(name,1,{"Water","tea leaves"}){}
 
+        //so it will take only one agrument and set default values with the help of main constructor.
+
         void displayTea(){
             cout<<"\nTea name : "<<teaName<<endl;
             cout<<"Servings : "<<servings<<endl;
@@ -41,6 +43,10 @@ int main(){
 
     Chai quickChai("Quick chai");
     quickChai.displayTea();
+
+    //to create object of chai need three parameters -- teaName,Servings,Ingredients
+    //but in some cases user want a quick tea so it just pass the first parameter to handle that delegation constructor are used.
+    //this is used in javascripts engine.(v8 engine)
 
 
 
