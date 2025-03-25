37db9326f619163559a121c49b13ec91a41bfd79
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-26 02:03:24 +0530

    update code and created main constructor for initializing objects

--- Day_11/Delegation.cpp
+++ Day_11/Delegation.cpp
@@ -3,8 +3,40 @@
 
 using namespace std;
 
+class Chai{
+    public:
+        string teaName;
+        int servings;
+        vector<string> ingredients;
+
+        //main constructor
+
+        Chai(string name,int serv,vector<string> ingre){
+            teaName = name;
+            servings = serv;
+            ingredients = ingre;
+
+            cout<<"Main constructor called !"<<endl;
+        }
+
+        void displayTea(){
+            cout<<"\nTea name : "<<teaName<<endl;
+            cout<<"Servings : "<<servings<<endl;
+            cout<<"Ingredients : ";
+            for (string str : ingredients)
+            {
+                cout<<str<<" ";
+            }
+            cout<<endl;
+            
+        }
+};
+
 int main(){
     cout<<"Delegation constructor";
+
+    Chai quickChai("Quick chai");
+    quickChai.displayTea();
 
 
 
