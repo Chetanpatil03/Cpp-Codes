7f9bf2dcfdfbf989797787c29a7b33742bb70e38
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-26 01:29:16 +0530

    Created class and constructor

--- Day_11/First.cpp
+++ Day_11/First.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
+
+class Chai{
+    private:
+        string teaName;
+        int servings;
+        vector<string> ingredieants;
+    public:
+        Chai(){
+            teaName = "Unknown tea";
+            servings = 1;
+            ingredieants = {"Water","Tea leaves"};
+        }
+        Chai(int serv,string name,vector<string>ingre){
+            teaName = name;
+            servings = serv;
+            ingredieants = ingre;
+        }
+};
 
 int main(){
     cout<<"Getter and setter methods";
