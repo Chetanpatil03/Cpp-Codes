54ba7ea57d00061a7f166ed401654c7ee6022af2
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-28 00:54:31 +0530

    updated created base class and functions of base class

--- Day_13/Inheritance.cpp
+++ Day_13/Inheritance.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
-class Demo1{
+//base,parent,original,super class
+class Tea{
+    protected:
+        //derived class allowed
+        string teaName;
+        int servings;
+    
 
     public:
-        void sayHello(){
-            cout<<"Class demo 1";
+        Tea(string name,int serv): teaName(name),servings(serv){
+            cout<<"Tea constructor called "<<teaName <<endl;
+        }
+
+        virtual void brewTea() const{
+            cout<<"Brewing "<<teaName<<"with generic method"<<endl;
+        }
+        virtual void servTea() const{
+            cout<<"Serving "<< servings<<" cups of tea "<<teaName<<"with generic method"<<endl;
+        }
+
+        ~Tea{
+            cout<<"Tea destructor called for "<<endl;
         }
 };
 
+//derived,child,sub,inherited class
 class child : public Demo1{
     
 };
@@ -17,7 +35,7 @@ class child : public Demo1{
 int main(){
     child ch;
     ch.sayHello();
-    
+
 
     return 0;
 }
