15fc01e52bf7afcdae389fd93fbd6a62bd63e898
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-27 23:59:41 +0530

    update

---
+++ Day_13/Inheritance.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Demo1{
+
+    public:
+        void sayHello(){
+            cout<<"Class demo 1";
+        }
+};
+class child : public Demo1{
+    
+};
+
+int main(){
+    child ch;
+    ch.sayHello();
+    return 0;
+}
