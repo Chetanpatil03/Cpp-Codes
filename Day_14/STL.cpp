310b120c652102aeb6a2cdcde2b7ce3adc3224cc
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-29 00:59:00 +0530

    update created structure and vector, deque

--- Day_14/STL.cpp
+++ Day_14/STL.cpp
@@ -24,7 +24,34 @@ struct Order
     int productID;
     int quantity;
     string customerID;
+    time_t orderDate;
 };
+
+struct Customer
+{
+    string customerID;
+    string customerName;
+
+};
+
+int main(){
+    vector<Product> products = {
+        {101,"Laptop","Electronics"},
+        {102,"SmartPhone","Electronics"},
+        {103,"I-Phone","Electronics"},
+        {104,"Coffee Maker","Kitchen"},
+        {105,"Blender","Kitchen"},
+        {106,"Desk Lamp","Home"}
+    };
+
+    deque<string>recentCustomers = {
+        "C001","C002","C003","C004","C005"
+    };
+
+
+    return 0;
+}
+
 
 
 
