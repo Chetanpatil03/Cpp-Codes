3fb840ac62b98b881505f064fae93c941914cddd
Chetan Patil <chetanpatil6303@gmail.com>
2025-04-02 00:30:36 +0530

    update created vector for storage of employees data

--- Day_15/Stl_part-ii.cpp
+++ Day_15/Stl_part-ii.cpp
@@ -16,3 +16,19 @@ struct Employee{
 void displayEmployee(const Employee& emp){
     cout<<"id : "<<emp.id<<", Name: "<<emp.name<<", Salary : $"<<emp.salary<<endl;
 }
+
+int main(){
+    vector<Employee> employees = { //containers for storage
+        {101,"Chetan",1000.00},
+        {102,"Hitesh",2000.00},
+        {103,"Shubam",900.00},
+        {104,"Prafull",950.00},
+        {105,"Dipak",1100.00}
+
+    };
+
+    //lambadas sorting
+    sort(employees.begin(),employees.end(), );
+
+
+}
